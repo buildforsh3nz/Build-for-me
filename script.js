663f12859803c256d2eb6d3061765f2f2c75b332
buildforsh3nz <droxyowo@gmail.com>
2025-06-12 09:33:47 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const message = document.createElement("p");
+    message.textContent = "GitHub Pages ile başarılı bir şekilde çalışıyor!";
+    document.body.appendChild(message);
+});
